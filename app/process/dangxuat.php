931d33495fd74ef9b35a1712bda3b4fb794efd71
tuanh24801 <tuanh12a12001@gmail.com>
2022-03-09 22:59:19 +0700

    created logout

--- app/process/dangxuat.php
+++ app/process/dangxuat.php
@@ -1,7 +1,10 @@
 <?php
+    // use SESSION
     session_start();
+
     if(isset($_SESSION['user_login'])){
         unset($_SESSION['user_login']);
         header('location: ../frontend/homepage.php');
     }
+
 ?>
