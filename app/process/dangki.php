87eaa641f70c88b05f40ed3516e091685d9d4bd0
tuanh24801 <tuanh12a12001@gmail.com>
2022-03-15 20:48:18 +0700

    update valid email _

--- app/process/dangki.php
+++ app/process/dangki.php
@@ -14,7 +14,7 @@
                 die;
         }
         
-        if(!preg_match('|^[A-Z0-9._]+@[A-Z0-9.-]+\.[A-Z]{2,4}$|i',$email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if(!preg_match('|^[A-Z0-9._]+@[A-Z0-9]+\.[A-Z]{2,4}$|i',$email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
                 echo "Định dạng mail không hợp lệ";
                 die;
         }
--- app/process/dangnhap.php
+++ app/process/dangnhap.php
@@ -9,7 +9,7 @@
                 echo 'Vui lòng điền thông tin email';
                 die;
         }
-        if(!preg_match('|^[A-Z0-9._]+@[A-Z0-9.-]+\.[A-Z]{2,4}$|i',$email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if(!preg_match('|^[A-Z0-9._]+@[A-Z0-9]+\.[A-Z]{2,4}$|i',$email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
                 echo "Định dạng mail không hợp lệ";
                 die;
         }
