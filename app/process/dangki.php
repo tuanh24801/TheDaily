da246c56083f90159e0a89f6e8bfa2784674d0b6
tuanh24801 <tuanh12a12001@gmail.com>
2022-03-15 20:40:29 +0700

    update valid email

--- app/process/dangki.php
+++ app/process/dangki.php
@@ -14,14 +14,11 @@
                 die;
         }
         
-        if(!preg_match('|^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$|i',$email)){
+        if(!preg_match('|^[A-Z0-9._]+@[A-Z0-9.-]+\.[A-Z]{2,4}$|i',$email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
                 echo "Định dạng mail không hợp lệ";
                 die;
         }
-	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo "Định dạng mail không hợp lệ";
-                die;
-        }
+
         if($password == ''){
                 echo 'vui lòng nhập mật khẩu lớn hơn 8 kí tự';
                 die;
